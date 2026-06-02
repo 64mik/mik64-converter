6220abf1a303be0e344e0711acbfe5249e922714
64mik-dev <64mik.dev@gmail.com>
2026-06-02 17:31:55 +0900

    feat: add append() and auto-indentation
    - update comment

--- h/mik64_converter.h
+++ h/mik64_converter.h
@@ -7,13 +7,14 @@ namespace mik64 {
     class Converter {
         public: 
             Converter() = delete;
-            Converter(const std::map<std::string, std::string>& templateMap);
+            Converter(const std::map<std::string, std::string>& templateMap, bool tagIndentation = true, int defaultIndentation = 0);
             ~Converter();
             std::ostringstream convert(const std::string& string);
             inline void setTemplateMap(const std::map<std::string, std::string>& templateMap) {
                 templateMap_ = templateMap;
             }
         private:
+            void append(std::ostringstream& oss, const std::string& str, int indent = 0);
             void replace(std::string& s, const std::string& key, const std::string& value);
             std::pair<std::string, std::string> getTag(const std::string& tag);
             std::map<std::string, std::string> templateMap_;
@@ -24,8 +25,9 @@ namespace mik64 {
                 std::string func_start_marker="(";
                 std::string func_end_marker=")";
                 std::string ignore_marker="//";
-                std::string meta_marker="---";
             } markers_;
+            bool tagIndentation_ = true;
+            int defaultIndentation_ = 0;
 };
 }
-#endif // MDPARSER_H
+#endif // CONVERTER_H
